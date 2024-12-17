2341cc4f83dc2a7dfe39e143494dcb42943b695d
Sandhyarani <chandaka.sandhyarani@capgemini.com>
2024-12-17 16:41:24 +0530

    first commit

---
+++ src/demogit/Simple.java
@@ -0,0 +1,10 @@
+package demogit;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		System.out.println("Good evening");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chasandh
+ *
+ */
+module GITDemo {
+}
