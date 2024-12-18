f30223549bc069d92956f14ce90696cf9da9d9b8
Sandhyarani <chandaka.sandhyarani@capgemini.com>
2024-12-18 09:55:34 +0530

    fourth commit

--- src/demogit/Simple.java
+++ src/demogit/Simple.java
@@ -5,6 +5,7 @@ public class Simple {
 	public static void main(String[] args) {
 		System.out.println("Good evening");
 		System.out.println("Good night");
+		System.out.println("create a new branch on 18/12/2024");
 
 	}
 
