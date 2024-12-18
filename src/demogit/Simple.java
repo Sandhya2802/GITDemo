07bf4b29f2a5064631c6082426705cb522fec20e
Sandhya2802 <chandaka.sandhyarani@capgemini.com>
2024-12-18 10:10:01 +0530

    Update Simple.java

--- src/demogit/Simple.java
+++ src/demogit/Simple.java
@@ -6,6 +6,7 @@ public class Simple {
 		System.out.println("Good evening");
 		System.out.println("Good night");
 		System.out.println("create a new branch on 18/12/2024");
+		System.out.println("create a new branch from github on 18/12/2024");
 
 	}
 
